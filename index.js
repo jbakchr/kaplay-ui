321ece0d8cf47214c4bbc98a66ccdefd88b7d168
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-03-24 19:43:24 +0100

    Removed shadow

--- index.js
+++ index.js
@@ -17,20 +17,10 @@ export const addTextButton = (txt, x, y, f = () => {}, hover = true) => {
     anchor("center"),
     outline(4),
     color(255, 255, 255),
-    z(10),
-  ]);
-
-  // Shadow effect
-  btn.add([
-    pos(x, y).add(3, 3),
-    rect(240, 80),
-    color(0, 50, 150),
-    anchor("center"),
-    z(5),
   ]);
 
   // button text
-  btn.add([text(txt), anchor("center"), color(0, 0, 0), z(15)]);
+  btn.add([text(txt), anchor("center"), color(0, 0, 0)]);
 
   btn.onClick(f);
 
