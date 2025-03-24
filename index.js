f6f3c3db9ec1b44c01564b27bbbb4736db1a80c5
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-03-24 12:17:46 +0100

    Added hover effect and updated version

--- index.js
+++ index.js
@@ -5,10 +5,11 @@ import "kaplay/global";
  * @param {number} x - The x postion to set.
  * @param {y} y - The y position to set.
  * @param {string} txt - Button text to display
- * @param {Function} [f] - Click event handler (default is () => {})) 
- * @returns {GameObj} 
+ * @param {Function} [f] - Click event handler (default is () => {}))
+ * @param {boolean} [hover] - Add hover effect (default is true)
+ * @returns {GameObj}
  */
-export const addTextButton = (x, y, txt, f = () => {}) => {
+export const addTextButton = (x, y, txt, f = () => {}, hover = true) => {
   const btn = add([
     rect(240, 80, { radius: 8 }),
     pos(x, y),
@@ -21,6 +22,20 @@ export const addTextButton = (x, y, txt, f = () => {}) => {
   btn.add([text(txt), anchor("center"), color(0, 0, 0)]);
 
   btn.onClick(f);
+
+  btn.onHoverUpdate(() => {
+    setCursor("pointer");
+    if (hover) {
+      btn.scale = vec2(1.1);
+    }
+  });
+
+  btn.onHoverEnd(() => {
+    setCursor("default");
+    if (hover) {
+      btn.scale = vec2(1);
+    }
+  });
 
   return btn;
 };
