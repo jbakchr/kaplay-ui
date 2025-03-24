d6eaf632bbecb1557d078647cb92c72a0a21236e
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-03-24 19:38:36 +0100

    Added shadow effect

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ import "kaplay/global";
 
 /**
  * Adds a clickable button with custom text
+ * @param {string} txt - Button text to display
  * @param {number} x - The x postion to set.
  * @param {y} y - The y position to set.
- * @param {string} txt - Button text to display
  * @param {Function} [f] - Click event handler (default is () => {}))
  * @param {boolean} [hover] - Add hover effect (default is true)
  * @returns {GameObj}
  */
-export const addTextButton = (x, y, txt, f = () => {}, hover = true) => {
+export const addTextButton = (txt, x, y, f = () => {}, hover = true) => {
   const btn = add([
     rect(240, 80, { radius: 8 }),
     pos(x, y),
@@ -17,9 +17,20 @@ export const addTextButton = (x, y, txt, f = () => {}, hover = true) => {
     anchor("center"),
     outline(4),
     color(255, 255, 255),
+    z(10),
   ]);
 
-  btn.add([text(txt), anchor("center"), color(0, 0, 0)]);
+  // Shadow effect
+  btn.add([
+    pos(x, y).add(3, 3),
+    rect(240, 80),
+    color(0, 50, 150),
+    anchor("center"),
+    z(5),
+  ]);
+
+  // button text
+  btn.add([text(txt), anchor("center"), color(0, 0, 0), z(15)]);
 
   btn.onClick(f);
 
