83741d7408ef721bb9b1fb582647412a5c2932eb
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-03-25 16:47:46 +0100

    Added icon button

--- buttons/index.js
+++ buttons/index.js
@@ -71,3 +71,73 @@ export const addTextButton = (
 
   return btn;
 };
+
+/**
+ * Adds a clickable button icon button with hover effect
+ * @param {string} spriteName - Sprite to use as icon
+ * @param {Function} [onClick] - Click event handler (default is () => {}))
+ * @param {number} x - The x postion to set (default is center().x).
+ * @param {number} y - The x postion to set (default is center().y)
+ * @param {number} w - Width of button (default is 48)
+ * @param {number} h - Height of button (default is 48)
+ * @param {boolean} [hoverFx] - Add hover effect (default is true)
+ * @returns {GameObj}
+ */
+export const addIconButton = (
+  spriteName,
+  onClick = () => {},
+  x = center().x,
+  y = center().y,
+  w = 48,
+  h = 48,
+  hoverFx = true
+) => {
+  // Shadow effect
+  const shadow = add([
+    rect(w, h, { radius: 8 }),
+    pos(x + 2, y + 2),
+    area(),
+    scale(1),
+    anchor("center"),
+    outline(1),
+    color(128, 128, 128),
+  ]);
+
+  // Add button
+  const iconBtn = add([
+    rect(w, h, { radius: 8 }),
+    pos(x, y),
+    area(),
+    scale(1),
+    anchor("center"),
+    outline(1),
+  ]);
+
+  // Add icon
+  iconBtn.add([sprite(spriteName), anchor("center")]);
+
+  // On click handler
+  iconBtn.onClick(() => {
+    setCursor("default");
+    onClick();
+  });
+
+  // On hover effect
+  iconBtn.onHover(() => {
+    setCursor("pointer");
+    if (hoverFx) {
+      iconBtn.scale = vec2(1.03);
+      shadow.scale = vec2(1.03);
+    }
+  });
+
+  iconBtn.onHoverEnd(() => {
+    setCursor("default");
+    if (hoverFx) {
+      iconBtn.scale = vec2(1);
+      shadow.scale = vec2(1);
+    }
+  });
+
+  return iconBtn;
+};
