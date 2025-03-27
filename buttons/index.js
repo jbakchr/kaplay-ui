cd37d588df893b0fe9dae538313c60aa127dd40e
Jonas Bak Phillipson <jbakchr@gmail.com>
2025-03-27 11:42:15 +0100

    Updated button

--- buttons/index.js
+++ buttons/index.js
@@ -1,67 +1,45 @@
 import "kaplay/global";
 
 /**
- * Adds a text button with shadow and hover effect
+ * Makes button with centered text
  * @param {string} txt - Button text to display
- * @param {number} x - The x postion to set (default is center().x).
- * @param {number} y - The x postion to set (default is center().y)
- * @param {number} width - Width of button (default is 120)
+ * @param {number} x - The x postion to set (default is 0).
+ * @param {number} y - The x postion to set (default is 0)
+ * @param {number} width - Width of button (default is 100)
  * @param {number} height - Height of button (default is 50)
  * @param {number} btnRadius - Border radius of button (default is 8)
  * @param {number} txtSize - Text size of button (default is 15)
- * @param {boolean} [hoverFx] - Add hover effect (default is true)
  * @returns {GameObj}
  */
-export const addTextButton = (
+export const makeTextButton = (
   txt,
-  x = center().x,
-  y = center().y,
-  width = 120,
+  x = 0,
+  y = 0,
+  width = 100,
   height = 50,
   btnRadius = 8,
-  txtSize = 15,
-  hoverFx = true
+  txtSize = 15
 ) => {
-  // Shadow effect
-  const shadow = add([
-    rect(width, height, { radius: btnRadius }),
-    pos(x + 2, y + 2),
-    area(),
-    scale(1),
-    anchor("center"),
-    outline(1),
-    color(128, 128, 128),
-  ]);
-
-  // Button
-  const btn = add([
+  // Make button
+  const btn = make([
     rect(width, height, { radius: btnRadius }),
     pos(x, y),
     area(),
     scale(1),
-    anchor("center"),
     outline(1),
     color(255, 255, 255),
   ]);
 
+  // Make button text
+  const btnText = make([
+    text(txt, { size: txtSize }),
+    pos(width / 2, height / 2),
+    anchor("center"),
+    color(0, 0, 0),
+  ]);
+
   // Button text
-  btn.add([text(txt, { size: txtSize }), anchor("center"), color(0, 0, 0)]);
-
-  btn.onHover(() => {
-    setCursor("pointer");
-    if (hoverFx) {
-      btn.scale = vec2(1.03);
-      shadow.scale = vec2(1.03);
-    }
-  });
-
-  btn.onHoverEnd(() => {
-    setCursor("default");
-    if (hoverFx) {
-      btn.scale = vec2(1);
-      shadow.scale = vec2(1);
-    }
-  });
+  btn.add(btnText);
 
   return btn;
 };
