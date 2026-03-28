91b750c2207d875744deb52f08613c2190c3e969
Jonas Bak Phillipson <jbakchr@gmail.com>
2026-03-28 12:39:06 +0100

    Added changing button text position based on button anchor changes

--- src/elements/text-button/text-button.ts
+++ src/elements/text-button/text-button.ts
@@ -7,7 +7,7 @@ import { TextButton } from "../../types";
 import { makeButton, makeText } from "../../components/";
 
 // Helpers
-import { getCenterPos } from "../../helpers";
+import { getCenterPos, setChildPosition } from "../../helpers";
 
 export function createTextButton(
   k: KAPLAYCtx,
@@ -16,7 +16,7 @@ export function createTextButton(
   height: number,
 ): TextButton {
   // Make button
-  let btn = makeButton(k, width, height);
+  const btn = makeButton(k, width, height);
 
   // Get center position of button to be used for positioning the button text in the center
   const { cX, cY } = getCenterPos(btn);
@@ -32,37 +32,8 @@ export function createTextButton(
   btn.onUse((id) => {
     // Button Anchor changes
     if (id === "anchor") {
-      switch (btn.anchor) {
-        case "bot":
-          btnTxt.use(k.pos(0 * cX, -1 * cY));
-          break;
-        case "botleft":
-          btnTxt.use(k.pos(1 * cX, -1 * cY));
-          break;
-        case "botright":
-          btnTxt.use(k.pos(-1 * cX, -1 * cY));
-          break;
-        case "center":
-          btnTxt.use(k.pos(0 * cX, 0 * cY));
-          break;
-        case "left":
-          btnTxt.use(k.pos(1 * cX, 0 * cY));
-          break;
-        case "right":
-          btnTxt.use(k.pos(-1 * cX, 0 * cY));
-          break;
-        case "top":
-          btnTxt.use(k.pos(0 * cX, 1 * cY));
-          break;
-        case "topleft":
-          btnTxt.use(k.pos(1 * cX, 1 * cY));
-          break;
-        case "topright":
-          btnTxt.use(k.pos(-1 * cX, 1 * cY));
-          break;
-        default:
-          break;
-      }
+      // Set new postion of Button Text (child)
+      setChildPosition(k, btn, btnTxt, cX, cY);
     }
   });
 
--- src/helpers/pos-utils.ts
+++ src/helpers/pos-utils.ts
@@ -1,4 +1,4 @@
-import { GameObj, PosComp, RectComp, AnchorComp } from "kaplay";
+import { GameObj, PosComp, RectComp, AnchorComp, KAPLAYCtx } from "kaplay";
 
 import { CenterPos } from "../types";
 
@@ -7,4 +7,44 @@ export function getCenterPos(
   gameObj: GameObj<RectComp | PosComp | AnchorComp>,
 ): CenterPos {
   return { cX: gameObj.width / 2, cY: gameObj.height / 2 };
+}
+
+export function setChildPosition(
+  k: KAPLAYCtx,
+  parentObj: GameObj<RectComp | PosComp | AnchorComp>,
+  childObj: GameObj<RectComp | PosComp | AnchorComp>,
+  cX: number,
+  cY: number,
+): void {
+  switch (parentObj.anchor) {
+    case "bot":
+      childObj.use(k.pos(0 * cX, -1 * cY));
+      break;
+    case "botleft":
+      childObj.use(k.pos(1 * cX, -1 * cY));
+      break;
+    case "botright":
+      childObj.use(k.pos(-1 * cX, -1 * cY));
+      break;
+    case "center":
+      childObj.use(k.pos(0 * cX, 0 * cY));
+      break;
+    case "left":
+      childObj.use(k.pos(1 * cX, 0 * cY));
+      break;
+    case "right":
+      childObj.use(k.pos(-1 * cX, 0 * cY));
+      break;
+    case "top":
+      childObj.use(k.pos(0 * cX, 1 * cY));
+      break;
+    case "topleft":
+      childObj.use(k.pos(1 * cX, 1 * cY));
+      break;
+    case "topright":
+      childObj.use(k.pos(-1 * cX, 1 * cY));
+      break;
+    default:
+      break;
+  }
 }
